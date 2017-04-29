2482614d64cd3bfeaa4c849887fb2053d4403427
Nick Moriarty <njmoriarty@gmail.com>
2017-04-29 13:38:54 +0100

    More terraingen

--- terraingen.py
+++ terraingen.py
@@ -1,5 +1,6 @@
 import math
 import random
+import numpy
 
 from game import *
 
@@ -15,7 +16,6 @@ def generate_fractal_heightmap(seed, length, max_height, ratio):
     while step > 1:
         offset = step >> 1
         for i in range(offset, elements, step):
-            print(i)
             assert( ary[i] is None )
             a = ary[i - offset]
             b = ary[i + offset]
@@ -27,3 +27,32 @@ def generate_fractal_heightmap(seed, length, max_height, ratio):
         error *= ratio # Drop the error
 
     return ary[0:length]
+
+def new_terrain_array(width, height):
+    return numpy.zeros((width, height), int)
+
+def rasterize_heightmap(terrain, heightmap, blocktype):
+    length = len(heightmap)
+    for x in range(0, length):
+        for y in range(0,math.ceil(heightmap[x])):
+            terrain[x,y] = blocktype
+
+def generate_planet_test(seed, width, height):
+    # Subseeding
+    r = random.Random(seed)
+    seed_rockbase = r.getrandbits(32)
+
+    # Generators
+    terrain = new_terrain_array(width, height)
+    rock_heightmap = generate_fractal_heightmap(
+        seed_rockbase, width, height / 2, 0.6)
+    rasterize_heightmap(terrain, rock_heightmap, 1)
+
+    return terrain
+
+def print_terrain(terrain, chars):
+    width, height = terrain.shape
+    for y in reversed(range(0,height)):
+        line = [ terrain[x, y] for x in range(0,width) ]
+        list = [ chars[int(i)] for i in line ]
+        print( ''.join(list) )
