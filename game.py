c36b072cced476d2c0b109136eaf8861e4fe62f3
Nick Moriarty <njmoriarty@gmail.com>
2017-04-29 11:16:04 +0100

    Add CAMERA_POSITION for looking (centering on) a point

--- game.py
+++ game.py
@@ -9,15 +9,21 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50, 50)
 
 pygame.init()
 
+CAMERA_POSITION = (0, 0)
 
 def world_to_screen_coordinates(world_coords):
-    return world_coords[0] * PPM, \
-           SCREEN_HEIGHT - world_coords[1] * PPM
-
+    window_coords = (   world_coords[0] - CAMERA_POSITION[0],
+                        world_coords[1] - CAMERA_POSITION[1] )
+    screen_coords = (   SCREEN_WIDTH  / 2 + window_coords[0] * PPM,
+                        SCREEN_HEIGHT / 2 - window_coords[1] * PPM )
+    return screen_coords
 
 def screen_to_world_coordinates(screen_coords):
-    return screen_coords[0] / PPM, \
-           (SCREEN_HEIGHT - screen_coords[1]) / PPM
+    window_coords = (   (SCREEN_WIDTH  / 2 + screen_coords[0]) / PPM,
+                        (SCREEN_HEIGHT / 2 - screen_coords[1]) / PPM )
+    world_coords  = (   window_coords[0] + CAMERA_POSITION[0],
+                        window_coords[1] + CAMERA_POSITION[1] )
+    return world_coords
 
 if __name__ == '__main__':
     pass
