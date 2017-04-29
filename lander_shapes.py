be70e329e07f417c1eb6eda1b05adffa46642285
becky <becky.naylor@simomics.com>
2017-04-29 12:56:07 +0100

    Added lander image.

--- lander_shapes.py
+++ lander_shapes.py
@@ -5,7 +5,7 @@ from GameObject import GameObject
 class Lander(GameObject):
 
     def __init__(self, world, position):
-        polygon_points = [[[0.39,0], [0.66,0], [1,0.3], [1,1], [0,1],[0,0.3]]]
+        polygon_points = [[0.39,0], [0.66,0], [1,0.3], [1,1], [0,1],[0,0.3]]
         circle_shapes = []
         image_path = "assets/lander.png"
         scale = 2
