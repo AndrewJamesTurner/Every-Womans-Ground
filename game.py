4cae8886a287411c4fedc26cc0ba5c1fadf09929
Nick Moriarty <njmoriarty@gmail.com>
2017-04-29 20:44:19 +0100

    Fix screen/window coords conversion

--- game.py
+++ game.py
@@ -21,9 +21,9 @@ def world_to_screen_coordinates(world_coords):
     screen_coords = (   SCREEN_WIDTH  / 2 + window_coords[0] * PPM,
                         SCREEN_HEIGHT / 2 - window_coords[1] * PPM )
     return screen_coords
-
+    
 def screen_to_world_coordinates(screen_coords):
-    window_coords = (   (SCREEN_WIDTH  / 2 + screen_coords[0]) / PPM,
+    window_coords = (   (screen_coords[0] - SCREEN_WIDTH / 2) / PPM,
                         (SCREEN_HEIGHT / 2 - screen_coords[1]) / PPM )
     world_coords  = (   window_coords[0] + CAMERA_POSITION[0],
                         window_coords[1] + CAMERA_POSITION[1] )
