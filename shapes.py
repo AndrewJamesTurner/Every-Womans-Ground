618392d0fc44e1060379430ad50b13e5ab3fc25e
Stuart Lacy <stuart.lacy@gmail.com>
2017-04-29 17:42:25 +0100

    Created Astronaut shape

--- shapes.py
+++ shapes.py
@@ -21,6 +21,24 @@ class LLeftShape(DynamicGameObject):
                                                    density, friction, restitution)
 
 
+class AstronautShape(DynamicGameObject):
+
+    def __init__(self, world, position):
+        polygon_points = [[[0.235, 0.307], [0.235, 0.071], [0.76, 0.071], [0.76, 0.307]],   # Head
+                          [[0.21, 0.307], [0.785, 0.307], [0.755, 0.764], [0.235, 0.764]],  # Trunk
+                          [[0.5, 0.787], [0.45, 0.933], [0.195, 0.933], [0.35, 0.764]],     # Left leg
+                          [[0.5, 0.787], [0.53, 0.933], [0.695, 0.933], [0.645, 0.764]]]    # Right leg
+        circle_shapes = []
+        image_path = os.path.join(constants.ASSETS_PATH, "astronaut_small.png")
+
+        scale = 2
+        density = 1
+        friction = 0.3
+        restitution = 0.4
+
+        self.body, self.image = self.prepare_shape(world, position, polygon_points, circle_shapes, image_path, scale,
+                                                   density, friction, restitution)
+
 class TerrainBlock(StaticGameObject):
     def __init__(self, world, position, blocktype):
         polygon_points = [[[0, 0], [0, 1], [1, 1], [1, 0]]]
