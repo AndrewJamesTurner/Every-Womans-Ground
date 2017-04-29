e1d8be870310a212ec0ce2c23f30e353cee72738
Stuart Lacy <stuart.lacy@gmail.com>
2017-04-29 17:42:58 +0100

    Created StationaryLander for planet exploration mode

--- lander_shapes.py
+++ lander_shapes.py
@@ -18,6 +18,21 @@ class Lander(DynamicGameObject):
 
         self.colour = (255,0,0,0)
 
+class StationaryLander(DynamicGameObject):
+
+    def __init__(self, world, position):
+        polygon_points = [[[0.39,0], [0.66,0], [1,0.3], [1,0.9], [0,0.9],[0,0.3]]]
+        circle_shapes = []
+        image_path = "assets/lander.png"
+        scale = 2
+
+        density = 20
+        friction = 1.0
+        restitution = 0.4
+
+        self.body, self.image = self.prepare_shape(world, position, polygon_points, circle_shapes, image_path, scale,
+                                                   density, friction, restitution)
+
 
 class PlanetGround(StaticGameObject):
 
