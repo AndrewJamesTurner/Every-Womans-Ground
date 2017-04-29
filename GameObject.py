c5c90e7084639fed6641392d9122d68986f6e8a2
Stuart Lacy <stuart.lacy@gmail.com>
2017-04-29 17:41:41 +0100

    Bug fix for collision detection

--- GameObject.py
+++ GameObject.py
@@ -30,6 +30,13 @@ class GameObject:
         :param screen:
         """
 
+        # Draw image for the body
+        if self.image is not None:
+            image_rect = self.image.get_rect()
+            rotated_image = pygame.transform.rotate(self.image, self.body.angle * 180 / math.pi)
+            rotated_image_rect = rotated_image.get_rect(center=world_to_screen_coordinates(self.body.position))
+            screen.blit(rotated_image, rotated_image_rect)
+
         # Draw box2d collision boxes of body
         if self.colour is not None:
             for fixture in self.body.fixtures:
@@ -37,12 +44,6 @@ class GameObject:
                 vertices = [world_to_screen_coordinates(self.body.transform * v) for v in shape.vertices]
                 pygame.draw.polygon(screen, self.colour, vertices)
 
-        # Draw image for the body
-        if self.image is not None:
-            image_rect = self.image.get_rect()
-            rotated_image = pygame.transform.rotate(self.image, self.body.angle * 180 / math.pi)
-            rotated_image_rect = rotated_image.get_rect(center=world_to_screen_coordinates(self.body.position))
-            screen.blit(rotated_image, rotated_image_rect)
 
     def prepare_shape(self, world, position, polygon_points, circle_shapes, image_path, scale,
                       density=1, friction=0.9, restitution=0.1):
