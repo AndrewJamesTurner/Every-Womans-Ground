8835e946b5ee4831e73eadf39be1dcf07ce4c4dd
Stuart Lacy <stuart.lacy@gmail.com>
2017-04-30 01:59:13 +0100

    Added health and fuel images

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import ezpygame
 from Box2D import b2World, b2PolygonShape
+import random
 
 import shapes
-from random import random
 from game import *
 
 
@@ -14,9 +14,16 @@ class DemoScene(ezpygame.Scene):
 
         self.world = b2World()  # default gravity is (0,-10) and doSleep is True
 
+        # terrain_locations = [(1, 0),
+        #                      (2, 0),
+        #                      (5, 0,),
+        #                      (10, 0)]
+        #
+        # self.terrain = shapes.create_terrain('water', terrain_locations, self.world)
+
         # Create an object that moves in the box2d world and can be rendered to the screen
-        self.many_shapes = [ shapes.LLeftShape(self.world, (random()*100 - 50, random()*30)) for x in range(1,100) ]
-        #self.demo_shape = shapes.LLeftShape(self.world, (5, 5))
+        #self.many_shapes = [ shapes.LLeftShape(self.world, (random()*100 - 50, random()*30)) for x in range(1,100) ]
+        self.demo_shape = shapes.LLeftShape(self.world, (5, 5))
 
         # A box2d object that doesn't move and isn't rendered to screen
         body_bottom_wall = self.world.CreateStaticBody(
@@ -34,15 +41,16 @@ class DemoScene(ezpygame.Scene):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 # Kick a shape
-                kick_shape = self.many_shapes[int(random() * len(self.many_shapes))]
-                kick_shape.body.ApplyLinearImpulse((0, 100), kick_shape.body.position, True)
+                #kick_shape = self.many_shapes[int(random() * len(self.many_shapes))]
+                self.demo_shape.body.ApplyLinearImpulse((0, 100), self.demo_shape.body.position, True)
 
     def draw(self, screen):
         # Called once per frame, to draw to the screen
 
         screen.fill(black)
-        for shape in self.many_shapes:
-            shape.draw(screen)
+        #for shape in self.many_shapes:
+        #    shape.draw(screen)
+        self.demo_shape.draw(screen)
 
     def update(self, dt):
         # Called once per frame, to update the state of the game
